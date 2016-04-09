722d41ee6aa9f11cec9a68fa670ee394fc2fd69d
Võ Đông Nhựt <nhutvd@asiantech.vn>
2016-04-09 18:13:31 +0700

    change database from mysql to sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -14,64 +14,64 @@
 ActiveRecord::Schema.define(version: 20160409083910) do
 
   create_table "api_keys", force: :cascade do |t|
-    t.string   "access_token", limit: 255,                null: false
-    t.integer  "customer_id",  limit: 4
-    t.boolean  "active",                   default: true, null: false
+    t.string   "access_token",                null: false
+    t.integer  "customer_id"
+    t.boolean  "active",       default: true, null: false
     t.datetime "expires_at"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  add_index "api_keys", ["access_token"], name: "index_api_keys_on_access_token", unique: true, using: :btree
-  add_index "api_keys", ["customer_id"], name: "index_api_keys_on_customer_id", using: :btree
+  add_index "api_keys", ["access_token"], name: "index_api_keys_on_access_token", unique: true
+  add_index "api_keys", ["customer_id"], name: "index_api_keys_on_customer_id"
 
   create_table "beers", force: :cascade do |t|
-    t.string   "name",         limit: 255
-    t.integer  "category_id",  limit: 4
-    t.string   "manufacturer", limit: 255
-    t.string   "country",      limit: 255
-    t.float    "price",        limit: 24
-    t.text     "description",  limit: 65535
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "name"
+    t.integer  "category_id"
+    t.string   "manufacturer"
+    t.string   "country"
+    t.float    "price"
+    t.text     "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  add_index "beers", ["category_id"], name: "index_beers_on_category_id", using: :btree
+  add_index "beers", ["category_id"], name: "index_beers_on_category_id"
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.string   "address",            limit: 255
-    t.string   "email",              limit: 255, default: "", null: false
-    t.string   "encrypted_password", limit: 255, default: "", null: false
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
+    t.string   "name"
+    t.string   "address"
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   limit: 255
-    t.string   "address",                limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
