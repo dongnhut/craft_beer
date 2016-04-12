cb6cf505f913f276fa9ad8a85aeaef15253f34c0
Võ Đông Nhựt <nhutvd@asiantech.vn>
2016-04-12 11:56:06 +0700

    added seed admin@gmail.com

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,6 +1,14 @@
 ActiveRecord::Base.transaction do
   User.destroy_all
   puts "Create admin ....."
+  
+  User.create!(
+      email: "admin@gmail.com",
+      name: FFaker::Name.name,
+      address: FFaker::Address.street_address + FFaker::Address.city,
+      password: "12345678",
+      )
+
   20.times do |i|
     User.create!(
       email: "admin#{i}@gmail.com",
